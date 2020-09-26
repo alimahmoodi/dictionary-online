c3d0069b0e78215cd04ae11c2f47371d7aa1372f
alimahmoodi <alimahmudi.alami@gmail.com>
2020-09-26 11:13:50 +0330

    this repo only use oline

---
+++ precache-manifest.60f17a50b322c9f244bb6298dee159ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16abcdfe042397c0705765125bb16183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81457c6275b47314e8d9",
+    "url": "/static/css/main.afb9aba2.chunk.css"
+  },
+  {
+    "revision": "8b1d364ce6e21f83d95f",
+    "url": "/static/js/2.2d34657a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2d34657a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81457c6275b47314e8d9",
+    "url": "/static/js/main.c0d816b4.chunk.js"
+  },
+  {
+    "revision": "43e90b86247cdd85b23e",
+    "url": "/static/js/runtime-main.45a4de75.js"
+  },
+  {
+    "revision": "2e2cad19c23737d70c5ad33e5d4db1c3",
+    "url": "/static/media/bin.2e2cad19.svg"
+  },
+  {
+    "revision": "e5ef37742fb08067527155958c3806a4",
+    "url": "/static/media/close-black.e5ef3774.svg"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/static/media/close.441d8611.svg"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/static/media/tick1.8b4d6e74.svg"
+  }
+]);
